90ee503a8556febbcfab8255641b8f108ceea148
Kanishka <kanishkakani648@gmail.com>
2026-06-26 19:34:50 +0530

    Add files via upload

---
+++ visula chart.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import os
+def generate_chart(cgpa_score, skill_score, project_score, certificate_score, internship_score):
+    scores = [
+        cgpa_score,
+        skill_score,
+        project_score,
+        certificate_score,
+        internship_score
+    ]
+    categories = [
+        "CGPA",
+        "Skills",
+        "Projects",
+        "Certificates",
+        "Internships"
+    ]
+    plt.figure(figsize=(6,6))
+    plt.pie(
+        scores,
+        labels=categories,
+        autopct="%1.1f%%"
+    )
+    plt.title("Placement Readiness Analysis")
+    image_location = os.path.join(
+        os.getcwd(),
+        "placement_readiness_chart.png"
+    )
+    plt.savefig(image_location)
+
+    plt.close()
+    return image_location
+location = generate_chart(
+    25,
+    30,
+    20,
+    5,
+    5
+)
+
+
+print("Chart saved at:", location)
